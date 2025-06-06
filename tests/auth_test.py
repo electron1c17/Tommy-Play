b9a00f07a4b90dfd4e233a389dc9df689c5db3e8
R.Namozov <namozovr178@gmail.com>
2025-06-06 19:40:38 +0500

    final stuff

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -5,7 +5,6 @@ from pages.home_page import HomePage
 def test_auth(driver_chrome):
     auth_page = AuthPage(driver_chrome)
     home_page = HomePage(driver_chrome)
-
     auth_page.auth_btn()
     auth_page.enter_login()
     auth_page.click_login_btn()
